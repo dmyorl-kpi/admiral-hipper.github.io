f48f0c7b55bdb9a0bf45a1483445245d0a57c246
admiral-hipper <61466217+admiral-hipper@users.noreply.github.com>
2020-03-08 18:10:10 +0200

    Add some changes

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+    $('.btn-menu').click(function(){
+        $('.menu-default').toggleClass('menu-after')
+        setTimeout('')
+        $('.article').toggleClass('article-after')
+    })
+   function O(obj){
+       return typeof obj=='object'?obj:document.getElementById(obj)
+   }
+    
+    
