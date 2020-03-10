4e24875c11aef8aeb607d9d608bba90d7ba317a5
admiral-hipper <61466217+admiral-hipper@users.noreply.github.com>
2020-03-10 20:59:52 +0200

    add

--- javascript.js
+++ javascript.js
@@ -1,11 +1,6 @@
 $('.btn-menu').click(function(){
         $('.menu-default').toggleClass('menu-after')
-<<<<<<< HEAD
         $('.article').toggleClass('article-after')
-=======
-        setTimeout('')
-        $('.info-user').toggleClass('info-after')
->>>>>>> 5daa6145b18c98643e01a314ae691ee9fc3d762b
     })
    function O(obj){
        return typeof obj=='object'?obj:document.getElementById(obj)
